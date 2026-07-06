350b03d50d5015baf9576624bbfe06217009a48c
Sheila Kimani <sheilanjerikimani@gmail.com>
2026-07-06 10:38:26 +0300

    Rename films to punchier titles, use reliable poster service

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,9 +10,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Film::create([
-            'title' => 'Dandora Nights',
-            'slug' => 'dandora-nights',
-            'poster_url' => 'https://picsum.photos/seed/dandora/600/900',
+            'title' => 'Buried',
+            'slug' => 'buried',
+            'poster_url' => 'https://picsum.photos/seed/buried2026/600/900',
             'logline' => 'A slum DJ unearths a fortune buried beneath his estate - and three nights to decide who deserves it.',
             'synopsis' => 'In the heart of Dandora, seventeen-year-old DJ Mtaa unearths a bag of unmarked cash while digging a foundation for his mothers new kiosk. As word spreads through the estate, old grudges resurface, a corrupt councilman circles, and Mtaa must choose between vanishing with the money or using it to save the community that raised him.',
             'cast' => 'Brian Ogana, Faith Wanjiku, Otieno Kadaghe',
@@ -21,9 +21,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Film::create([
-            'title' => 'Route 46',
-            'slug' => 'route-46',
-            'poster_url' => 'https://picsum.photos/seed/route46/600/900',
+            'title' => 'Last Stop Nairobi',
+            'slug' => 'last-stop-nairobi',
+            'poster_url' => 'https://picsum.photos/seed/laststop2026/600/900',
             'logline' => 'One matatu. One unforgettable rush hour. A citys worth of stories collide.',
             'synopsis' => 'On a gridlocked Thika Road afternoon, a matatu crew - a hustling conductor, a nervous first-time driver, and a busload of strangers - become entangled in a proposal gone wrong, a runaway bride, and a robbery in progress, all before the sun sets over Nairobis skyline.',
             'cast' => 'Njoki Karanja, Kevin Mwangi, Aisha Bakari',
@@ -32,9 +32,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Film::create([
-            'title' => 'Tides of Lamu',
-            'slug' => 'tides-of-lamu',
-            'poster_url' => 'https://picsum.photos/seed/lamu/600/900',
+            'title' => 'The Inheritance',
+            'slug' => 'the-inheritance',
+            'poster_url' => 'https://picsum.photos/seed/inheritance2026/600/900',
             'logline' => 'She left Lamu at seventeen to escape a secret. Twenty years later, the tide brings it home.',
             'synopsis' => 'When acclaimed architect Amina Sharif returns to her childhood home on Lamu Island to bury her estranged father, she uncovers letters revealing the real reason she was sent away - and a family debt that threatens to swallow the islands last dhow-building family.',
             'cast' => 'Amina Rashid, Salim Bakari, Grace Njeri',
